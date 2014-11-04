01feb117af1960054a8be4e31f7e50f9b51ecfd5
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2014-11-04 13:11:03 -0500

    Add a loading message when you first hit the page.

--- client/views/servers_list.js
+++ client/views/servers_list.js
@@ -1,7 +1,13 @@
 /*global Meteor, Template, Session, Servers */
 
+var serversHandle = Meteor.subscribe('servers');
+
 Template.serversList.servers = function() {
   return Servers.find({}, {sort: {name: 1}});
+};
+
+Template.serversList.loading = function() {
+  return !serversHandle.ready();
 };
 
 Template.serversList.events({
