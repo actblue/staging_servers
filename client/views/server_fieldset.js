df9b74072cafa60eb4f8ce1631cf62bdcff8b632
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2015-11-11 21:48:57 -0500

    Convert server_fieldset from CoffeeScript to ES6.

---
+++ client/views/server_fieldset.js
@@ -0,0 +1,21 @@
+Template.serverFieldset.helpers({
+
+  hasDefaultUrlPattern() {
+    return Meteor.settings &&
+      Meteor.settings.public &&
+      Meteor.settings.public.urlPattern;
+  },
+
+  defaultUrl() {
+    if (Meteor.settings &&
+        Meteor.settings.public &&
+        Meteor.settings.public.urlPattern) {
+      let urlPattern = Meteor.settings.public.urlPattern;
+      let name = AutoForm.getFieldValue('name');
+      let replacementString = name ? name : '%';
+      return urlPattern.replace('%', replacementString);
+    } else {
+      return '';
+    }
+  }
+});
