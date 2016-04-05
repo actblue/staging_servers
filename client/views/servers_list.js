fc1a46c770f407a31f73665bbf2cc214579bcf5b
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2016-04-05 14:00:43 -0400

    Convert servers_list from CoffeeScript to ES6.

---
+++ client/views/servers_list.js
@@ -0,0 +1,18 @@
+Template.serversList.helpers({
+  servers() {
+    return Servers.find({}, { sort: { name: 1 } });
+  }
+});
+
+Template.serversList.events({
+  'click .new_server'() {
+    if (!Meteor.userId()) {
+      return;
+    }
+    return openCreateServerDialog();
+  }
+});
+
+const openCreateServerDialog = () => {
+  Session.set('showCreateServerDialog', true);
+}
